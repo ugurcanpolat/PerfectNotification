5ea4ca25c62af3c6cf6089a48a61fcac12f350c1
Ugurcan Polat <polatu@itu.edu.tr>
2017-07-06 15:59:07 +0300

    FCM implementation for Android push notifications

--- Sources/main.swift
+++ Sources/main.swift
@@ -106,49 +106,48 @@ class NotificationsHandler {
         }
     }
     
-    // Not operatable yet
-//    func notifyAndroidDevices(request: HTTPRequest, response: HTTPResponse) {
-//        print("Sending notification to Android devices.")
-//        
-//        let data = request.postBodyString!.data(using: .utf8)
-//        var pushDictionary = [String: Any]()
-//        
-//        do {
-//            pushDictionary = try JSONSerialization.jsonObject(with: data!, options: []) as! [String: Any]
-//        } catch {
-//            print("Empty request body.")
-//            response.completed()
-//            return
-//        }
-//        
-//        var FCMRequest = URLRequest(url: URL(string: androidFCMSendUrl)!)
-//        FCMRequest.httpMethod = "POST"
-//        FCMRequest.setValue("application/application/json", forHTTPHeaderField: "Content-Type")
-//        FCMRequest.setValue("key=\(androidServerKey)", forHTTPHeaderField: "Authorization")
-//        
-//        var bodyOfRequest = "{ \"notification\": {\n\t\"title\": \"\(pushDictionary["title"] as! String)\",\n"
-//        bodyOfRequest += "\t\"body\": \"\(pushDictionary["body"] as! String)\"\n  },\n"
-//        bodyOfRequest += "  \"to\": \"\(pushDictionary["to"] as! String)\"\n}"
-//        
-//        FCMRequest.httpBody = bodyOfRequest.data(using: .utf8)
-//        
-//        let task = URLSession.shared.dataTask(with: FCMRequest) { (data, response, error) in
-//            guard let data = data, error == nil else {
-//                // Check for fundamental networking errors
-//                return
-//            }
-//            
-//            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
-//                return
-//            }
-//            
-//            let responseFromFCM = String(data: data, encoding: .utf8)
-//            print(responseFromFCM ?? "")
-//        }
-//        // Resume the task since it is in the suspended state when it is created
-//        task.resume()
-//        response.completed()
-//    }
+    func notifyAndroidDevices(request: HTTPRequest, response: HTTPResponse) {
+        print("Sending notification to Android devices.")
+        
+        let data = request.postBodyString!.data(using: .utf8)
+        var pushDictionary = [String: Any]()
+        
+        do {
+            pushDictionary = try JSONSerialization.jsonObject(with: data!, options: []) as! [String: Any]
+        } catch {
+            print("Empty request body.")
+            response.completed()
+            return
+        }
+        
+        var FCMRequest = URLRequest(url: URL(string: androidFCMSendUrl)!)
+        FCMRequest.httpMethod = "POST"
+        FCMRequest.setValue("application/application/json", forHTTPHeaderField: "Content-Type")
+        FCMRequest.setValue("key=\(androidServerKey)", forHTTPHeaderField: "Authorization")
+        
+        var bodyOfRequest = "{ \"notification\": {\n\t\"title\": \"\(pushDictionary["title"] as! String)\",\n"
+        bodyOfRequest += "\t\"body\": \"\(pushDictionary["body"] as! String)\"\n  },\n"
+        bodyOfRequest += "  \"to\": \"\(pushDictionary["to"] as! String)\"\n}"
+        
+        FCMRequest.httpBody = bodyOfRequest.data(using: .utf8)
+        
+        let task = URLSession.shared.dataTask(with: FCMRequest) { (data, response, error) in
+            guard let data = data, error == nil else {
+                // Check for fundamental networking errors
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+                return
+            }
+            
+            let responseFromFCM = String(data: data, encoding: .utf8)
+            print(responseFromFCM ?? "")
+        }
+        // Resume the task since it is in the suspended state when it is created
+        task.resume()
+        response.completed()
+    }
 }
 
 var handler = NotificationsHandler()
@@ -157,7 +156,7 @@ let routes = [
     Route(method: .post, uri: "/add/{deviceid}", handler: handler.receiveDeviceId),
     Route(method: .get, uri: "/notify", handler: handler.notifyDevices),
     Route(method: .post, uri: "/notify", handler: handler.notifyDevices),
-    //Route(method: .post, uri: "/notifyAndroid", handler: handler.notifyAndroidDevices),
+    Route(method: .post, uri: "/notifyAndroid", handler: handler.notifyAndroidDevices),
     Route(method: .get, uri: "/list", handler: handler.listDeviceIds)
 ]
 
